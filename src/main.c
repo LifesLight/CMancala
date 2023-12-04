d569e27fc3e16f515f90e66f77943930e5c49803
Alexander Kurtz <alexkurtz@web.de>
2023-12-04 15:17:50 +0100

    Basic utility added

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * Board is constructed like this:
@@ -15,12 +16,15 @@
 
 struct Board
 {
-    uint8_t* cells[14];
+    uint8_t cells[14];
     bool turn;
 };
 
-struct Board* new_board() {
-    struct Board* b = malloc(sizeof(struct Board));
+typedef struct Board Board;
+
+
+Board* newBoard() {
+    struct Board* b = malloc(sizeof(Board));
     // Check if allocation failed
     if (b == NULL) {
         return NULL;
@@ -40,8 +44,75 @@ struct Board* new_board() {
     return b;
 }
 
+// Just dont question it
+void renderBoard(const Board *b) {
+    printf("    ┌─");
+    for (int i = 0; i < 5; ++i) {
+        printf("──┬─");
+    }
+    printf("──┐\n");
+
+    printf("┌───┤");
+    for (int i = 12; i > 7; --i) {
+        printf("%2d │", b->cells[i]);
+    }
+    printf("%2d ├───┐\n", b->cells[7]);
+
+    printf("│%2d ├─", b->cells[SCORE_P2]);
+    for (int i = 0; i < 5; ++i) {
+        printf("──┼─");
+    }
+    printf("──┤%2d │\n", b->cells[SCORE_P1]);
+
+    printf("└───┤");
+    for (int i = 0; i < 5; ++i) {
+        printf("%2d │", b->cells[i]);
+    }
+    printf("%2d ├───┘\n", b->cells[5]);
+
+    printf("    └─");
+    for (int i = 0; i < 5; ++i) {
+        printf("──┴─");
+    }
+    printf("──┘\n");
+}
+
+// Returns if recieved second move
+bool makeMoveOnBoard(Board* b, uint8_t index) {
+    // Propagate stones
+    uint8_t stones = b->cells[index];
+    b->cells[index] = 0;
+    while (stones > 0) {
+        index++;
+        index %= 14;
+        b->cells[index]++;
+        stones--;
+    }
+
+    // Check if last stone was placed on score field
+    if (index == SCORE_P1 || index == SCORE_P2) {
+        return true;
+    }
+
+    // Check if last stone was placed on empty field
+    // If yes "Steal" stones
+    if (b->cells[index] == 1) {
+        int targetIndex = 12 - index;
+        // Branchless computation for who gets the stones
+        // TODO: Check if this actually works :D
+        b->cells[SCORE_P1 + (SCORE_P2 - SCORE_P1) * (index / (SCORE_P1 + 1))] += b->cells[targetIndex] + 1;
+        b->cells[targetIndex] = 0;
+        b->cells[index] = 0;
+    }
+
+    // Return no extra move
+    return false;
+}
 
 int main(int argc, char const *argv[])
 {
+    Board* myBoard = newBoard();
+    renderBoard(myBoard);
+    free(myBoard);
     return 0;
 }
