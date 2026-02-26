44d5ff9155822e30947e4b8106abb7690267be2f
Alexander Kurtz <alexkurtz@web.de>
2026-02-26 12:45:56 +0100

    Merged throughput.c and solver.c into algo.c

--- src/logic/solver/algo.c
+++ src/logic/solver/algo.c
@@ -2,17 +2,50 @@
  * Copyright (c) Alexander Kurtz 2026
  */
 
-#include "logic/solver/solver.h"
+#include "logic/solver/algo.h"
 
-extern void aspirationRoot_GLOBAL_CLASSIC(Context* context, SolverConfig *config);
-extern void aspirationRoot_GLOBAL_AVALANCHE(Context* context, SolverConfig *config);
-extern void aspirationRoot_LOCAL_CLASSIC(Context* context, SolverConfig *config);
-extern void aspirationRoot_LOCAL_AVALANCHE(Context* context, SolverConfig *config);
+int64_t nodeCount;
 
-extern void distributionRoot_GLOBAL_CLASSIC(Board *board, int32_t* distribution, bool *solved, SolverConfig *config);
-extern void distributionRoot_GLOBAL_AVALANCHE(Board *board, int32_t* distribution, bool *solved, SolverConfig *config);
-extern void distributionRoot_LOCAL_CLASSIC(Board *board, int32_t* distribution, bool *solved, SolverConfig *config);
-extern void distributionRoot_LOCAL_AVALANCHE(Board *board, int32_t* distribution, bool *solved, SolverConfig *config);
+// 1. INSTANTIATE LOCAL SOLVER CLASSIC (With Cache)
+// FN(negamax) -> negamax_LOCAL_CLASSIC
+#define PREFIX LOCAL_CLASSIC
+#define SOLVER_USE_CACHE 1
+#define MAKE_MOVE makeMoveOnBoardClassic
+#include "logic/solver/impl/solver_template.h"
+#undef PREFIX
+#undef SOLVER_USE_CACHE
+#undef MAKE_MOVE
+
+// 2. INSTANTIATE LOCAL SOLVER AVALANCHE (With Cache)
+// FN(negamax) -> negamax_LOCAL_AVALANCHE
+#define PREFIX LOCAL_AVALANCHE
+#define SOLVER_USE_CACHE 1
+#define MAKE_MOVE makeMoveOnBoardAvalanche
+#include "logic/solver/impl/solver_template.h"
+#undef PREFIX
+#undef SOLVER_USE_CACHE
+#undef MAKE_MOVE
+
+// 3. INSTANTIATE GLOBAL SOLVER CLASSIC (No Cache)
+// FN(negamax) -> negamax_GLOBAL_CLASSIC
+#define PREFIX GLOBAL_CLASSIC
+#define SOLVER_USE_CACHE 0
+#define MAKE_MOVE makeMoveOnBoardClassic
+#include "logic/solver/impl/solver_template.h"
+#undef PREFIX
+#undef SOLVER_USE_CACHE
+#undef MAKE_MOVE
+
+// 4. INSTANTIATE GLOBAL SOLVER AVALANCHE (No Cache)
+// FN(negamax) -> negamax_GLOBAL_AVALANCHE
+#define PREFIX GLOBAL_AVALANCHE
+#define SOLVER_USE_CACHE 0
+#define MAKE_MOVE makeMoveOnBoardAvalanche
+#include "logic/solver/impl/solver_template.h"
+#undef PREFIX
+#undef SOLVER_USE_CACHE
+#undef MAKE_MOVE
+
 
 void aspirationRoot(Context* context, SolverConfig *config) {
     switch (config->solver) {
