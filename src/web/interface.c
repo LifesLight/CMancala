86f118bd3afc296981f949c69a59bfddd2664514
Alexander Kurtz <alexkurtz@web.de>
2026-03-06 15:07:17 +0100

    addition to prev

--- src/web/interface.c
+++ src/web/interface.c
@@ -992,7 +992,8 @@ EM_JS(void, launch_gui, (const char* v_ptr), {
     
     modalHtml += "<p>EGDB generation algorithm inspired by <a href='https://github.com/girving/kalah' target='_blank'>girving/kalah</a>:</p>";
     modalHtml += "<div class='license-box'>";
-    modalHtml += "Copyright 2009, Geoffrey Irving.<br>All rights reserved.<br><br>";
+    modalHtml += "<strong>BSD License</strong><br><br>";
+    modalHtml += "Copyright 2009, Geoffrey Irving. All rights reserved.<br><br>";
     modalHtml += "Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:<br><br>";
     modalHtml += "1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.<br>";
     modalHtml += "2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.<br>";
@@ -1000,15 +1001,25 @@ EM_JS(void, launch_gui, (const char* v_ptr), {
     modalHtml += "THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.";
     modalHtml += "</div>";
 
-    modalHtml += "<p style='margin-top:15px'>This software uses <strong>Zstandard (zstd)</strong>.</p>";
+    modalHtml += "<p style='margin-top:15px'>This software uses <strong>Zstandard (zstd)</strong>:</p>";
     modalHtml += "<div class='license-box'>";
-    modalHtml += "BSD License<br>For Zstandard software<br><br>";
+    modalHtml += "<strong>BSD License</strong><br><br>";
+    modalHtml += "For Zstandard software<br>";
     modalHtml += "Copyright (c) Meta Platforms, Inc. and affiliates. All rights reserved.<br><br>";
     modalHtml += "Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:<br><br>";
-    modalHtml += "* Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.<br>";
-    modalHtml += "* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.<br>";
-    modalHtml += "* Neither the name Facebook, nor Meta, nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.<br><br>";
-    modalHtml += "THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS &quot;AS IS&quot; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.";
+    modalHtml += " * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.<br>";
+    modalHtml += " * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.<br>";
+    modalHtml += " * Neither the name Facebook, nor Meta, nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.<br><br>";
+    modalHtml += "THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.";
+    modalHtml += "</div>";
+
+    modalHtml += "<p style='margin-top:15px'>Compiled with <strong>Emscripten</strong>:</p>";
+    modalHtml += "<div class='license-box'>";
+    modalHtml += "<strong>MIT License</strong><br><br>";
+    modalHtml += "Copyright (c) 2010-2014 Emscripten authors, see AUTHORS file.<br><br>";
+    modalHtml += "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:<br><br>";
+    modalHtml += "The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.<br><br>";
+    modalHtml += "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
     modalHtml += "</div>";
 
     modalHtml += "<button onclick='window.closeAbout(event)' class='modal-close-btn'>Close</button>";
